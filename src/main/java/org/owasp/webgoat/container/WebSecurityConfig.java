aebe608924cfe48b7b442bac785ff94b8faeb631
ivaaanr7 <ivanricoinstituto@gmail.com>
2026-02-26 18:49:26 +0100

    Update WebSecurityConfig.java
    añado las librerias

--- src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
+++ src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
@@ -43,6 +43,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 /** Security configuration for WebGoat. */
 @Configuration
 @AllArgsConstructor
